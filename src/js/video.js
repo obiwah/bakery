4b032036532146359e57e6adadb8f5e4ab541fac
obiwah <a.v.kuz@inbox.ru>
2020-03-05 16:16:10 +0700

    a commit

--- src/js/video.js
+++ src/js/video.js
@@ -2,6 +2,18 @@
 
 document.addEventListener(`DOMContentLoaded`, function () { //wait for document load
 
+	const advantagesPlay = document.querySelector(`.advantages__play`);
+	const advantagesControls = document.querySelector(`.advantages__controls`);
+	const advantagesSwitcher = document.querySelector(`.advantages__play-pause`);
+	const advantagesTimeTotalMin = document.querySelector(`.advantages__time--total-min`);
+	const advantagesTimeTotalSec = document.querySelector(`.advantages__time--total-sec`);
+	const advantagesThumb = document.querySelector(`.advantages__thumb`);
+	const advantagesProgressNow = document.querySelector(`.advantages__progress-now`);
+	let advantagesTimeNowMin = document.querySelector(`.advantages__time--now-min`);
+	let advantagesTimeNowSec = document.querySelector(`.advantages__time--now-sec`);
+	let timerInterval, thumbInterval;
+	let player;
+
 	// YouTube IFrame Player API
 
 	// 2. This code loads the IFrame Player API code asynchronously.
@@ -10,24 +22,12 @@ document.addEventListener(`DOMContentLoaded`, function () { //wait for document 
 	tag.src = "https://www.youtube.com/iframe_api";
 	let firstScriptTag = document.getElementsByTagName('script')[0];
 	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-	// document.body.insertAdjacentElement('beforeend', tag);
-
-	// 3. This function creates an <iframe> (and YouTube player)
-	//    after the API code downloads.
-	const advantagesVideo = document.getElementById(`advantages-video`);
-	const advantagesPlay = document.querySelector(`.advantages__play`);
-	const advantagesControls = document.querySelector(`.advantages__controls`);
-	const advantagesSwitcher = document.querySelector(`.advantages__play-pause`);
-	const advantagesTimeTotal = document.querySelector(`.advantages__time--total`);
-	const advantagesThumb = document.querySelector(`.advantages__thumb`);
-	const advantagesProgressNow = document.querySelector(`.advantages__progress-now`);
-	let advantagesTimeNow = document.querySelector(`.advantages__time--now`);
-	let timerInterval, thumbInterval;
-	let player;
 
 	advantagesPlay.onclick = onYouTubeIframeAPIReady;
 	// advantagesSwitcher.addEventListener(`click`, onYouTubeIframeAPIReady, {once: true});
 
+	// 3. This function creates an <iframe> (and YouTube player)
+	//    after the API code downloads.
 	function onYouTubeIframeAPIReady() {
 		// advantagesSwitcher.removeEventListener(`click`, onYouTubeIframeAPIReady, {once: true});
 		advantagesControls.style.top = `-5px`;
@@ -37,8 +37,7 @@ document.addEventListener(`DOMContentLoaded`, function () { //wait for document 
 			videoId: 'COwlqqErDbY',
 			// videoId: 'jwywG820naw',
 			playerVars: {
-				// 'autoplay': 0,
-				// 'controls': 0
+				'controls': 0
 			},
 			events: {
 				'onReady': onPlayerReady,
@@ -49,23 +48,23 @@ document.addEventListener(`DOMContentLoaded`, function () { //wait for document 
 
 	// 4. The API will call this function when the video player is ready.
 	function onPlayerReady(event) {
-		showPause();
+		showTotalTime();
 
 		advantagesSwitcher.onclick = playPauseSwitcher;
 
-		showTime(advantagesTimeTotal, event.target.getDuration());
-
 		event.target.setVolume(5);
+		showPause();
 		event.target.playVideo();
 
-		startTimer();
-		thumbMover(event);
+		timeStarter();
+		thumbMover();
 	}
 
 	function onPlayerStateChange(event) {
+		// console.log(event.data);
 		if (event.data === YT.PlayerState.PLAYING) {
 			showPause();
-			startTimer();
+			timeStarter();
 			thumbMover();
 		}
 
@@ -80,20 +79,20 @@ document.addEventListener(`DOMContentLoaded`, function () { //wait for document 
 			clearInterval(timerInterval);
 			clearInterval(thumbInterval);
 		}
-
 	}
 
 	function playPauseSwitcher () {
 		if (advantagesSwitcher.classList.contains(`video__play-pause--play`)) {
 			showPause();
 			player.playVideo();
-			startTimer();
+			timeStarter();
 		}
 
-		if 	(advantagesSwitcher.classList.contains(`video__play-pause--pause`)) {
+		if (advantagesSwitcher.classList.contains(`video__play-pause--pause`)) {
 			showPlay();
 			player.pauseVideo();
 			clearInterval(timerInterval);
+			clearInterval(thumbInterval);
 		}
 
 		if (advantagesSwitcher.classList.contains(`video__play-pause--replay`)) {
@@ -111,41 +110,39 @@ document.addEventListener(`DOMContentLoaded`, function () { //wait for document 
 
 	function showPause() {
 		advantagesSwitcher.classList.remove(`video__play-pause--play`);
+		advantagesSwitcher.classList.remove(`video__play-pause--replay`);
 		advantagesSwitcher.classList.add(`video__play-pause--pause`);
 	}
 
 	function showReplay() {
 		advantagesSwitcher.classList.remove(`video__play-pause--play`);
+		advantagesSwitcher.classList.remove(`video__play-pause--pause`);
 		advantagesSwitcher.classList.add(`video__play-pause--replay`);
-
 	}
 
-	function showTime(elem, time) {
-		elem.innerHTML = ``;
-		elem.innerHTML += Math.floor(time / 60);
-		elem.innerHTML += `:`;
-		elem.innerHTML += (`0` + (time - Math.floor(time / 60) * 60)).slice(-2) ;
+	function showTotalTime() {
+		advantagesTimeTotalMin.innerHTML = Math.floor(player.getDuration() / 60);
+		advantagesTimeTotalSec.innerHTML = (`0` + (player.getDuration() - Math.floor(player.getDuration() / 60) * 60)).slice(-2);
 	}
 
-	function startTimer () {
+	function timeStarter () {
 		timerInterval = setInterval(() => {
-			console.log(`timer`);
-			advantagesTimeNow.innerHTML = ``;
-			advantagesTimeNow.innerHTML += Math.floor(Math.round(player.getCurrentTime()) / 60);
-			advantagesTimeNow.innerHTML += `:`;
-			advantagesTimeNow.innerHTML += (`0` + (Math.ceil(player.getCurrentTime()) - Math.floor(player.getCurrentTime() / 60) * 60)).slice(-2);
+			console.log(`timer: `, timerInterval);
+			advantagesTimeNowMin.innerHTML = Math.floor(Math.round(player.getCurrentTime()) / 60);
+			advantagesTimeNowSec.innerHTML = (`0` + (Math.ceil(player.getCurrentTime())
+																												- Math.floor(player.getCurrentTime() / 60) * 60)).slice(-2);
 		}, 1000);
 	}
 
-	function thumbMover (event) {
+	function thumbMover () {
 		thumbInterval = setInterval(() => {
-			console.log(`thumbMover`);
+			console.log(`thumbMover: ` + thumbInterval);
 			advantagesProgressNow.style.width = `${(player.getCurrentTime() / player.getDuration() * 100 * 0.61)}%`;
 			advantagesThumb.style.left = 8.7 + Math.floor(player.getCurrentTime() / player.getDuration() * 100 * 0.6) + `%`;
 
 	// 8.8 - 0
 	// 67.8 - 100
-		}, 50);
+		}, 1000);
 
 	}
 });
